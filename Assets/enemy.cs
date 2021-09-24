50fa7a72679d1000c45996c3dae21ccf435cdd40
Benjamin Northrop <521982@escg.ac.uk>
2021-09-24 16:12:52 +0100

    UPdate

---
+++ Assets/enemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+
+
+    public GameObject player;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float px = player.transform.position.x;
+        print(px);
+
+    
+    }
+}
