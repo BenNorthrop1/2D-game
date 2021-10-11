6e76695fefa0e677ae2a031142fcfaa1ba78a87a
BenNorthrop1 <521982@chrome.escg.ac.uk>
2021-10-11 15:42:15 +0100

    update

--- Assets/Scripts/PLAYERMOVEMENT.cs
+++ Assets/Scripts/PLAYERMOVEMENT.cs
@@ -26,6 +26,7 @@ public class PLAYERMOVEMENT : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         regular = speed;
+        isGrounded = false;
     }
 
     // Update is called once per frame
@@ -33,23 +34,25 @@ public class PLAYERMOVEMENT : MonoBehaviour
     {
         m_Animation = gameObject.GetComponent<Animator>();
 
-
+        DoCollisons();
         DoJump();
         DoMove();
         DoFight();
-        DoCollisons();
+        
         
 
     }
 
     void DoCollisons()
     {
-        float rayLength = 1f;
+        float rayLength = 0.2f;
 
 
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayLength);
 
         Color hitColor = Color.white;
+
+        isGrounded = false;
 
         if (hit.collider != null)
         {
@@ -57,170 +60,165 @@ public class PLAYERMOVEMENT : MonoBehaviour
 
             if (hit.collider.tag == "Ground")
             {
-                print("player has hit the ground");
                 hitColor = Color.green;
                 isGrounded = true;
             }
-
-            else
-            {
-                isGrounded = false;
-            }
-        }
-        Debug.DrawRay(transform.position, -Vector2.up * rayLength, hitColor);
-    }
-
-
-
-
-    void DoJump()
-    {
-        Vector2 velocity = rb.velocity;
-
-        // check for jump
-        if (Input.GetKey("space")&& isGrounded)
-        {
-            if (velocity.y < 0.01f)
-            {
-                velocity.y = jumpheight;
-                m_Animation.SetTrigger("Jumping");
-            }
-        }
-
-        rb.velocity = velocity;
-
-    }
-
-
-    
-    
-    
-
-
-
-
-
-
-
-   /* private void OnCollisionStay2D(Collision2D collision)
-    {
-        isGrounded = true;
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        isGrounded = false;
-    }
-   */
-
-    void DoFaceLeft(bool faceleft)
-    {
-        if (faceleft == true)
-        {
-            transform.localRotation = Quaternion.Euler(0, 180 , 0);
-        }
-        else
-        {
-            transform.localRotation = Quaternion.Euler(0, 0 , 0);
-        }
-    }
-    
-    
-
-    
-    
-    
-    
-    void DoMove()
-    {
-        Vector2 velocity = rb.velocity;
-
-        // stop player sliding when not pressing left or right
-        velocity.x = 0;
-
-        // check for moving left
-        if (Input.GetKey("a"))
-        {
-            velocity.x = -speed;
-            m_Animation.SetBool("IsMoving", true);
-        }
-        
-        // check for moving right
-        else if (Input.GetKey("d"))
-        {
-            velocity.x = speed;
-            m_Animation.SetBool("IsMoving", true);
-        }
-        else
-        {
-            m_Animation.SetBool("IsMoving", false);
-        }
-
-        if (Input.GetButton("Sprint"))
-        {
-            speed = sprint;
-        }
-        else
-        {
-            speed = regular;
-        }
-
-
-        if (velocity.x < -0.5f)
-        {
-            Helper.DoFaceLeft(gameObject , true);
-        }
-        if (velocity.x > 0.5f)
-        {
-            Helper.DoFaceLeft(gameObject , false);
-        }
-
-
-
-        rb.velocity = velocity;
-
-    }
-
-
-
-    void CreateRock()
-    {
-
-            int dir = Helper.GetObjectDir( gameObject );
             
-            
-            if(dir==Right)       // get the player direction
+
+            Debug.DrawRay(transform.position, -Vector2.up * rayLength, hitColor);
+        }
+
+    }
+
+
+        void DoJump()
+        {
+            Vector2 velocity = rb.velocity;
+
+            // check for jump
+            if (Input.GetKey("space") && isGrounded)
             {
-                Helper.MakeBullet(projectile, transform.position.x , transform.position.y+1 , 16 , 1);
+                if (velocity.y < 0.01f)
+                {
+                    velocity.y = jumpheight;
+                    m_Animation.SetTrigger("Jumping");
+                }
+            }
+
+            rb.velocity = velocity;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        /* private void OnCollisionStay2D(Collision2D collision)
+         {
+             isGrounded = true;
+         }
+
+         private void OnCollisionExit2D(Collision2D collision)
+         {
+             isGrounded = false;
+         }
+        */
+
+        void DoFaceLeft(bool faceleft)
+        {
+            if (faceleft == true)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
             }
             else
             {
-                Helper.MakeBullet(projectile, transform.position.x + 1f , transform.position.y + 1 , -16 , 1);
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
             }
-    }
-
-
-
-
-
-    void DoFight()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            m_Animation.SetTrigger("Fight");
         }
 
-    }
-
-    void Health()
-    {
-        
-    }
 
 
 
 
 
 
+        void DoMove()
+        {
+            Vector2 velocity = rb.velocity;
 
+            // stop player sliding when not pressing left or right
+            velocity.x = 0;
+
+            // check for moving left
+            if (Input.GetKey("a"))
+            {
+                velocity.x = -speed;
+                m_Animation.SetBool("IsMoving", true);
+            }
+
+            // check for moving right
+            else if (Input.GetKey("d"))
+            {
+                velocity.x = speed;
+                m_Animation.SetBool("IsMoving", true);
+            }
+            else
+            {
+                m_Animation.SetBool("IsMoving", false);
+            }
+
+            if (Input.GetButton("Sprint"))
+            {
+                speed = sprint;
+            }
+            else
+            {
+                speed = regular;
+            }
+
+
+            if (velocity.x < -0.5f)
+            {
+                Helper.DoFaceLeft(gameObject, true);
+            }
+            if (velocity.x > 0.5f)
+            {
+                Helper.DoFaceLeft(gameObject, false);
+            }
+
+
+
+            rb.velocity = velocity;
+
+        }
+
+
+
+        void CreateRock()
+        {
+
+            int dir = Helper.GetObjectDir(gameObject);
+
+
+            if (dir == Right)       // get the player direction
+            {
+                Helper.MakeBullet(projectile, transform.position.x, transform.position.y + 1, 16, 1);
+            }
+            else
+            {
+                Helper.MakeBullet(projectile, transform.position.x + 1f, transform.position.y + 1, -16, 1);
+            }
+        }
+
+
+
+
+
+        void DoFight()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                m_Animation.SetTrigger("Fight");
+            }
+
+        }
+
+        void Health()
+        {
+
+        }
+
+
+
+
+
+
+    
 }
